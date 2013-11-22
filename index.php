476766769b22e34c95d3a34aad4d2051197c3bac
supgeek-rod <zhanghooyo@gmail.com>
2013-11-23 00:21:41 +0800

    basic version
    this version 1, basic version

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * 项目入口文件
+ * living: more crazy, more beautful
+ */
+
+define("APP_DEBUG",true);
+define("APP_NAME","Home");
+define("APP_PATH","./");
+require("./ThinkPHP/ThinkPHP.php");
